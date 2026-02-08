607eabef7b5a23caa23d8ce1b653a03368d54468
Rithveckh <rithveckhdhamodharan@gmail.com>
2026-02-08 22:52:35 +0530

    super admin setup

--- app/api/create-user/route.ts
+++ app/api/create-user/route.ts
@@ -1,3 +1,40 @@
+// import { NextResponse } from "next/server"
+// import { createClient } from "@supabase/supabase-js"
+
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_KEY!
+// )
+
+// export async function POST(req:Request){
+
+//   const { clerk_id, email, name } = await req.json()
+
+//   // check if user exists
+//   const { data:existing } = await supabase
+//     .from("users")
+//     .select("*")
+//     .eq("clerk_id", clerk_id)
+//     .single()
+
+//   if(existing){
+//     return NextResponse.json({status:"exists"})
+//   }
+
+//   // default tenant (change later)
+//   const defaultTenant = "b8329d58-356d-4c46-a4be-e18a032892d2"
+
+//   await supabase.from("users").insert([{
+//     clerk_id,
+//     email,
+//     name,
+//     role:"resident",
+//     tenant_id:defaultTenant
+//   }])
+
+//   return NextResponse.json({status:"created"})
+// }
+
 import { NextResponse } from "next/server"
 import { createClient } from "@supabase/supabase-js"
 
@@ -10,7 +47,7 @@ export async function POST(req:Request){
 
   const { clerk_id, email, name } = await req.json()
 
-  // check if user exists
+  // check existing
   const { data:existing } = await supabase
     .from("users")
     .select("*")
@@ -21,15 +58,23 @@ export async function POST(req:Request){
     return NextResponse.json({status:"exists"})
   }
 
-  // default tenant (change later)
-  const defaultTenant = "b8329d58-356d-4c46-a4be-e18a032892d2"
+  // ⭐ YOUR SUPER ADMIN EMAIL
+  const SUPER_ADMIN_EMAIL = "ridham4113@gmail.com"
+
+  let role="resident"
+  let tenant_id="demo1"
+
+  if(email === SUPER_ADMIN_EMAIL){
+    role="superadmin"
+    tenant_id="master"
+  }
 
   await supabase.from("users").insert([{
     clerk_id,
     email,
     name,
-    role:"resident",
-    tenant_id:defaultTenant
+    role,
+    tenant_id
   }])
 
   return NextResponse.json({status:"created"})
